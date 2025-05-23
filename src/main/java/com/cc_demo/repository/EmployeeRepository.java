a29687001ae33c13f07882d4eec3f944a4b9240e
Tushar Soni <tusharsoni142001@gmail.com>
2025-05-23 13:55:23 +0530

    Added find by email method

--- src/main/java/com/cc_demo/repository/EmployeeRepository.java
+++ src/main/java/com/cc_demo/repository/EmployeeRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     List<Employee> findByDepartment(String department);
+
+    Employee findByEmail(String email);
 }
